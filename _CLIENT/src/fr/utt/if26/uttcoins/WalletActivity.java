c8b435ff66e9f1a232e32b31b9ed96dfb6e43086
Julien Calderan <julien.calderan@gmail.com>
2013-12-12 20:25:42 +0100

    ajout du wallet

---
+++ _CLIENT/src/fr/utt/if26/uttcoins/WalletActivity.java
@@ -0,0 +1,22 @@
+package fr.utt.if26.uttcoins;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class WalletActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_wallet);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.wallet, menu);
+		return true;
+	}
+
+}
