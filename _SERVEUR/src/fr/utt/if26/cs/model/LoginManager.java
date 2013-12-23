096ea7233207b167a93014781365a45c64214df8
steven salaun <steven.salaun@gmail.com>
2013-12-23 22:15:54 +0100

    quick fix token generator

--- _SERVEUR/src/fr/utt/if26/cs/model/LoginManager.java
+++ _SERVEUR/src/fr/utt/if26/cs/model/LoginManager.java
@@ -2,7 +2,8 @@ package fr.utt.if26.cs.model;
 
 public class LoginManager {
 	
-	private static String dico = "abcdefghijklmnopqrstwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@$";
+	private final static String dico = "abcdefghijklmnopqrstwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@$";
+	private final static int tokenSize = 30;
 	
 	public LoginManager(){
 		
@@ -11,8 +12,8 @@ public class LoginManager {
 	public static String generateToken(){
 		String token="";
 		int max = dico.length()-1;
-		for(int i=0; i<30; i++){
-			int random = (int) Math.random()*max;
+		for(int i=0; i<LoginManager.tokenSize; i++){
+			int random = (int) (Math.random()*max);
 			token += dico.charAt(random);
 		}
 		return token;
