89edcc22a51aa4bc0eb27bcf79c825ca47e2e6c8
steven salaun <steven.salaun@gmail.com>
2014-01-10 23:25:49 +0100

    Authentification pour /Transaction 	modified:   model/LoginManager.java 	modified:   servlets/TransactionServlet.java

--- _SERVEUR/src/fr/utt/if26/cs/servlets/TransactionServlet.java
+++ _SERVEUR/src/fr/utt/if26/cs/servlets/TransactionServlet.java
@@ -1,7 +1,6 @@
 package fr.utt.if26.cs.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -10,11 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.bson.BSONObject;
 import org.bson.BasicBSONObject;
 import org.bson.types.ObjectId;
-
-import com.mongodb.util.JSON;
 
 import fr.utt.if26.cs.database.Database;
 import fr.utt.if26.cs.database.DatabaseManager;
@@ -22,6 +18,7 @@ import fr.utt.if26.cs.exceptions.BeanException;
 import fr.utt.if26.cs.io.Echo;
 import fr.utt.if26.cs.io.JsonEcho;
 import fr.utt.if26.cs.model.DataBean;
+import fr.utt.if26.cs.model.LoginManager;
 import fr.utt.if26.cs.model.Transaction;
 import fr.utt.if26.cs.model.User;
 import fr.utt.if26.cs.utils.ServletUtils;
@@ -44,49 +41,40 @@ public class TransactionServlet extends HttpServlet {
 		Echo out = new JsonEcho(response.getWriter());
 		Database dbTransactions = DatabaseManager.getInstance().getBase(DatabaseManager.TRANSACTIONS);
 		BasicBSONObject params = ServletUtils.extractRequestData(ServletUtils.GET, request);
-		if(params.containsField("id")){
-			if(ObjectId.isValid(params.getString("id"))){
-				dbTransactions.open();
-				Transaction transaction=null;
-				try {
-					transaction = (Transaction) dbTransactions.getBean("id", params.getString("id"));
-				} catch (BeanException e) {
-					out.echo(e.getMessage());
-					e.printStackTrace();
+		if(LoginManager.checkAuth(params)){
+			if(params.containsField("id")){
+				if(ObjectId.isValid(params.getString("id"))){
+					dbTransactions.open();
+					Transaction transaction=null;
+					try {
+						transaction = (Transaction) dbTransactions.getBean("id", params.getString("id"));
+					} catch (BeanException e) {
+						out.echo(e.getMessage());
+						e.printStackTrace();
+					}
+					dbTransactions.close();
+					out.echo(transaction.getJSONStringRepresentation());
 				}
-				dbTransactions.close();
-				out.echo(transaction.getJSONStringRepresentation());
-			}
-			else{
-				out.echo("{'error':'invalid_id'}");
-			}
-		} 
-		else {
-			if(ServletUtils.checkRequiredFields(new String[]{"tag"}, params)){
-				try {
-					User userFrom = UserUtils.getUserFromTag(params.getString("tag"));
-					ArrayList<DataBean>[] transactions = TransactionsUtils.getUserTransactions(userFrom);
-					out.echo(TransactionsUtils.toBSON(transactions));
+				else{
+					out.echo("{'error':'invalid_id'}");
+				}
+			} 
+			else {
+				if(ServletUtils.checkRequiredFields(new String[]{"tag"}, params)){
+					try {
+						User userFrom = UserUtils.getUserFromTag(params.getString("tag"));
+						ArrayList<DataBean>[] transactions = TransactionsUtils.getUserTransactions(userFrom);
+						out.echo(TransactionsUtils.toBSON(transactions));
+					} catch (BeanException e) {
+						out.echo(e.getMessage());
+						e.printStackTrace();
+					}
 					
-					/*out.echo("{ \n\t'balance' : "+TransactionsUtils.computeTransactions(transactions)+",");
-					out.echo("\t'from': [");
-					for(DataBean t : transactions[0]){
-						System.out.echo(JSON.parse(t.toString()));
-						out.echo("\t\t"+t.getJSONStringRepresentation()+",");
-					}
-					out.echo("\t],"); 
-					out.echo("\t'to': [");
-					for(DataBean t : transactions[1]){
-						out.echo("\t\t"+t.getJSONStringRepresentation()+",");
-					}
-					out.echo("\t]\n}");*/
-				} catch (BeanException e) {
-					out.echo(e.getMessage());
-					e.printStackTrace();
 				}
-				
 			}
 		}
+		else
+			out.echo("auth required");
 	}
 
 	/**
@@ -95,20 +83,24 @@ public class TransactionServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Echo out = new JsonEcho(response.getWriter());
 		BasicBSONObject params = ServletUtils.extractRequestData(ServletUtils.POST, request);
-		if(ServletUtils.checkRequiredFields(new String[] {"from", "to", "amount"}, params)){
-			Transaction transaction=null;
-			try {
-				transaction = new Transaction(
-					params.getInt("amount"), 
-					params.getString("from"),
-					params.getString("to")
-				);
-				TransactionsUtils.doTransaction(transaction);
-				out.echo(transaction.getJSONStringRepresentation());
-			} catch (BeanException e1) {
-				out.echo(e1.getMessage());
-				e1.printStackTrace();
+		if(LoginManager.checkAuth(params)){
+			if(ServletUtils.checkRequiredFields(new String[] {"email", "to", "amount"}, params)){
+				Transaction transaction=null;
+				try {
+					transaction = new Transaction(
+						params.getInt("amount"), 
+						params.getString("email"),
+						params.getString("to")
+					);
+					TransactionsUtils.doTransaction(transaction);
+					out.echo(transaction.getJSONStringRepresentation());
+				} catch (BeanException e1) {
+					out.echo(e1.getMessage());
+					e1.printStackTrace();
+				}
 			}
 		}
+		else
+			out.echo("auth required");
 	}
 }
